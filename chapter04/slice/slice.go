4166000b0fbdf2d680a9e002bf41714acdf2cf1d
edfeff <396859442@qq.com>
2019-10-13 12:27:09 +0800

    add 切片的创建、遍历、追加、二维切片等代码

--- chapter04/slice/slice.go
+++ chapter04/slice/slice.go
@@ -56,5 +56,116 @@ func Show() {
 	ss1[1] = 100
 	fmt.Println(ss1)
 
+	//使用切片创建切片
+	ss2 := ss1[1:3]
+	ss2[0] = 999
+	ss2[1] = 999
+	fmt.Println(ss1)
+	fmt.Println(len(ss1))
+	fmt.Println(ss2)
+	fmt.Println(len(ss2))
 
+	//切片扩容
+	//长度和容量都是5
+	ss3 := []int{1, 2, 3, 4, 5}
+	//长度是2，容量是 5-1 =4
+	ss4 := ss3[1:3]
+	fmt.Println(ss4)
+	//使用append会修改切片的尺寸
+	ss4 = append(ss4, 6)
+	ss4 = append(ss4, 7)
+	ss4 = append(ss4, 8)
+	ss4 = append(ss4, 9)
+	ss4 = append(ss4, 10)
+	ss4 = append(ss4, 11)
+	ss4 = append(ss4, 12)
+	ss4 = append(ss4, 13)
+
+	//使用append时，也会修改关联的切片
+	fmt.Println(ss4) //[2 3 6 7 8 9 10 11 12 13]
+	fmt.Println(ss3) //[1 2 3 6 7]
+
+	//限制切片的容量
+	//5个元素
+	ss5 := []string{"a", "b", "c", "d", "e"}
+	//使用3个索引，分别是 [开始位置,结束位置) 最终位置
+	//实际长度 = 结束位置-开始位置
+	//实际容量 = 最终位置 - 开始位置
+	//长度为1 容量为2的
+	//这样会在修改时，发现新的切片和原有切片共用了开始位置和结束位置之间的内存，
+	// 如果共享位置的值发生了修改，新切片和旧切片的影响都是相同的，
+	//如果新切片的长度超出了指定的共享位置，那么就不再和就切片有联系
+	ss6 := ss5[2:3:4]
+	ss6[0] = "X"
+	ss6 = append(ss6, "f")
+	ss6 = append(ss6, "g")
+	fmt.Println(ss6) //[X f g]
+	fmt.Println(ss5) //[a b X f e]
+	//所以，新旧切片出现了共享空间就会发生一些列诡异的问题，所以要避免这样使用
+	//一般将新切片的结束位置等于最种位置，这样当使用append函数时，go会开辟一段新的内存地址
+
+	ss7 := ss5[2:3:3]
+	ss7 = append(ss7, "7")
+	ss7 = append(ss7, "7")
+	fmt.Println(ss5) //[a b X f e]
+	fmt.Println(ss7) //[X 7 7]
+
+	//切片追加
+	ss8 := []int{1, 2}
+	ss9 := []int{3, 4}
+	ss10 := append(ss8, ss9...)
+	fmt.Println(ss8)
+	fmt.Println(ss9)
+	fmt.Println(ss10)
+
+	//迭代切片
+	//range
+	ss11 := []int{10, 20, 30, 40}
+	for index, value := range ss11 {
+		fmt.Println(index, value)
+	}
+	//不使用索引
+	for _, value := range ss11 {
+		fmt.Println(value)
+	}
+	//不使用值
+	for index, _ := range ss11 {
+		fmt.Println(index)
+	}
+	//range 提供的是副本
+	//比较下内存地址
+	for index, value := range ss11 {
+		fmt.Printf("%X,%X \n", &value, &ss11[index])
+	}
+	//传统for循环,根精确的索引控制
+	for index := 1; index < len(ss11); index += 2 {
+		fmt.Println(ss11[index])
+	}
+	//切片的长度和容量
+	//len
+	//cap
+	fmt.Println(len(ss11))
+	fmt.Println(cap(ss11))
+
+	//多维切片
+	m1 := [][]int{{10}, {100, 200}}
+	fmt.Println(m1)
+
+	//组合切片的切片
+	m1[0] = append(m1[0], 20)
+	fmt.Println(m1)
+
+	//在函数种使用切片参数
+	f1()
+}
+func f1() {
+	s := make([]int, 1e6)
+	//函数间传递推荐使用切片，因为切片本身的结构就3部分，头指针，长度，容量，
+	// 所以当作参数传递比较简单，比较节省空间
+	s2 := f2(s)
+	fmt.Println(len(s2))
+	fmt.Println(cap(s2))
+}
+func f2(slice []int) []int {
+	return slice
 }
